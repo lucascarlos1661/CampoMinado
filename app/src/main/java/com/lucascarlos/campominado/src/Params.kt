4ef1999eeee175e8645d99a98ceb358a81ecd213
Lucas Carlos <lucascarlos1661@gmail.com>
2022-04-30 20:37:38 -0300

    Refatorando classe Params, deixando funcionalidade mais simples e retirando funções que não eram necessárias

--- app/src/main/java/com/lucascarlos/campominado/src/Params.kt
+++ app/src/main/java/com/lucascarlos/campominado/src/Params.kt
@@ -7,20 +7,28 @@ import kotlin.math.floor
 class Params {
 
     private val blockSize: Int = 30
-    private val headerRatio: Double = 0.15
-    private val marginHorizontalBoard = 5
-    private val marginBottomBoard = 6
-    private var percentageOfMines: Double = 0.0
 
-    //The difficulty level will be calculated through the option selected by the user
-    //Mine percentage is fixed. 7% for beginner, 13% for Intermediate and 20% for Expert
+    // The height, width, number of rows and columns of the board are calculated according to the user's screen
+    // The width of the board is equal to the total width of the user's screen minus margin horizontal
+    private val marginHorizontalBoard = 5
+
+    private var boardWidthInDp: Float =
+        Resources.getSystem().displayMetrics.run { (widthPixels / density) - marginHorizontalBoard }
+
+    // The height of the board is fixed at 85% of the total height of the user's screen
+    private val boardProportion = 0.85F
+
+    private var boardHeightInDp: Float =
+        Resources.getSystem().displayMetrics.run { ((heightPixels / density) * boardProportion) }
+
+    // The difficulty level will be calculated through the option selected by the user
+    // Mine percentage is fixed. 7% for beginner, 13% for Intermediate and 20% for Expert
+
+    private var percentageOfMines: Double = 0.0
 
     private val difficultLevelBeginner: Double = 0.07
     private val difficultLevelIntermediate: Double = 0.13
     private val difficultLevelExpert: Double = 0.2
-
-    private var screenWidthInDp: Float = 0F
-    private var screenHeightInDp: Float = 0F
 
     fun getMinesAmount(gameDifficultSelected: Int): Int {
         when (gameDifficultSelected) {
@@ -38,30 +46,10 @@ class Params {
     }
 
     fun getColumnsAmount(): Int {
-        val width: Float = getScreenWidth() - marginHorizontalBoard
-        return floor((width / blockSize).toDouble()).toInt()
+        return floor(boardWidthInDp / blockSize).toInt()
     }
 
     fun getRowsAmount(): Int {
-        val totalHeight = getScreenHeight()
-        val boardHeight = totalHeight * (1 - headerRatio) - marginBottomBoard
-        return floor(boardHeight / blockSize).toInt()
-    }
-
-    private fun getScreenWidth(): Float {
-        val widthPixels: Int = Resources.getSystem().displayMetrics.widthPixels
-        screenWidthInDp = convertPixelsToDp(widthPixels)
-        return screenWidthInDp
-    }
-
-    private fun getScreenHeight(): Float {
-        val heightPixels: Int = Resources.getSystem().displayMetrics.heightPixels
-        screenHeightInDp = convertPixelsToDp(heightPixels)
-        return screenHeightInDp
-    }
-
-    private fun convertPixelsToDp(px: Int): Float {
-        val systemDensityDpi: Int = Resources.getSystem().displayMetrics.densityDpi
-        return px / (systemDensityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
+        return floor(boardHeightInDp / blockSize).toInt()
     }
 }
